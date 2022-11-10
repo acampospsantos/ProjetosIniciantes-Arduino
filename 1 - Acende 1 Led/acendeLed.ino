d7c05ad0994fa729bba7aad0fcd15370bd9e941f
antsu comuna <acampospsantos@hotmail.com>
2022-11-10 15:19:07 -0300

    Código - 1

--- 1 - Acende 1 Led/acendeLed.ino
+++ 1 - Acende 1 Led/acendeLed.ino
@@ -9,6 +9,7 @@ void loop(){
     //Apaga o Led
     digitalWrite(13, LOW);
     delay(600); // Wait for 600 millisecond(s)
+    
     //Acende o Led
     digitalWrite(13, HIGH);
     delay(600); // Wait for 600 millisecond(s)
