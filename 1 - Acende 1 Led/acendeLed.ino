faeba0cfeefcc0e45147603b19b04147b752cc72
antsu comuna <acampospsantos@hotmail.com>
2022-11-10 14:58:56 -0300

    Código - 1

---
+++ 1 - Acende 1 Led/acendeLed.ino
@@ -0,0 +1,15 @@
+void setup(){
+  pinMode(13, OUTPUT);
+  //Led começa aceso
+  digitalWrite(13, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+}
+
+void loop(){
+    //Apaga o Led
+    digitalWrite(13, LOW);
+    delay(600); // Wait for 600 millisecond(s)
+    //Acende o Led
+    digitalWrite(13, HIGH);
+    delay(600); // Wait for 600 millisecond(s)
+}
